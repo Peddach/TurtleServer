09332de92f9b15ac0fd0a85b366acea3b9b2f4c5
Professor Sam <76458774+ProfessorSam@users.noreply.github.com>
2022-08-06 02:06:15 +0200

    Add playermanagment with MongoDB (#2)
    Add player managment via MongoDB and Morphia

---
+++ src/main/java/de/petropia/turtleServer/server/commands/PlayerCommand.java
@@ -0,0 +1,118 @@
+package de.petropia.turtleServer.server.commands;
+
+import de.petropia.turtleServer.server.TurtleServer;
+import de.petropia.turtleServer.server.user.PetropiaPlayer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+public class PlayerCommand implements CommandExecutor, TabCompleter {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!(sender instanceof Player bukkitPlayer)){
+            return false;
+        }
+        if(!bukkitPlayer.hasPermission("TurtleServer.command.player")){
+            bukkitPlayer.sendMessage(Component.text("Keine Rechte").color(NamedTextColor.RED));
+            return false;
+        }
+        if(args.length == 0){
+            bukkitPlayer.sendMessage(Component.text("Subcommands: Cache, Info"));
+            return true;
+        }
+        if(args.length == 1 && args[0].equalsIgnoreCase("Cache")){
+            sendMessage(bukkitPlayer, Component.text("Cached players"));
+            for(PetropiaPlayer player : TurtleServer.getMongoDBHandler().getChachedPlayers()){
+                printPlayerInfo(player, bukkitPlayer);
+            }
+            TurtleServer.getInstance().getMessageSender().sendMessage(bukkitPlayer, Component.text("Spieler im cache: " + TurtleServer.getMongoDBHandler().getChachedPlayers().size()));
+        }
+        if(args[0].equalsIgnoreCase("info")){
+            if(args.length == 1){
+                sendMessage(bukkitPlayer, Component.text("Bitte gib an, ob du die uuid angeben willst oder den Nutzernamen"));
+                sendMessage(bukkitPlayer, Component.text("Bedenke, Nutzernamen ändern sich, UUIDs bleiben immer gleich"));
+                return false;
+            }
+            if(args.length == 3 && args[1].equalsIgnoreCase("name")){
+                TurtleServer.getMongoDBHandler().getPetropiaPlayerByUsername(args[2]).thenAccept(player -> {
+                   if(player == null){
+                       sendMessage(bukkitPlayer, Component.text("Spieler nicht gefunden: " + args[2]).color(NamedTextColor.RED));
+                       return;
+                   }
+                   printPlayerInfo(player, bukkitPlayer);
+                });
+            }
+            else if(args.length == 3 && args[1].equalsIgnoreCase("uuid")){
+                TurtleServer.getMongoDBHandler().getPetropiaPlayerByUUID(args[2]).thenAccept(player -> {
+                    if(player == null){
+                        sendMessage(bukkitPlayer, Component.text("Spieler nicht gefunden: " + args[2]).color(NamedTextColor.RED));
+                        return;
+                    }
+                   printPlayerInfo(player, bukkitPlayer);
+                });
+            }
+            else {
+                TurtleServer.getInstance().getMessageSender().sendMessage(bukkitPlayer, Component.text("Bitte gib einen Namen oder eine uuid an!").color(NamedTextColor.RED));
+            }
+        }
+        return false;
+    }
+
+    private void printPlayerInfo(PetropiaPlayer petropiaPlayer, Player sender){
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("ID: " + petropiaPlayer.getId().toHexString()));
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("UUID: " + petropiaPlayer.getUuid()));
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("NAME: " + petropiaPlayer.getUserName()));
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("ONLINE: " + petropiaPlayer.isOnline()));
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("SERVER: " + petropiaPlayer.getServer()));
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("LAST ONLINE: " + petropiaPlayer.getLastOnline()));
+        TurtleServer.getInstance().getMessageSender().sendMessage(sender, Component.text("HISTORY: " + nameHistoryAsString(petropiaPlayer)));
+    }
+
+    private String nameHistoryAsString(PetropiaPlayer petropiaPlayer) {
+        StringBuilder string = new StringBuilder();
+        for(String name : petropiaPlayer.getNameHistory()){
+            string.append(name).append(", ");
+        }
+        return string.toString();
+    }
+
+    private void sendMessage(CommandSender sender, Component message){
+        if(sender instanceof Player player){
+            TurtleServer.getInstance().getMessageSender().sendMessage(player, message);
+            return;
+        }
+        sender.sendMessage(message);
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!sender.hasPermission("TurtleServer.command.player")){
+            return null;
+        }
+        List<String> complete = new ArrayList<>();
+        if(args.length == 1){
+            StringUtil.copyPartialMatches(args[0], Arrays.asList("cache", "info"), complete);
+        }
+        if(args.length == 2 && args[0].equalsIgnoreCase("info")){
+            StringUtil.copyPartialMatches(args[1], Arrays.asList("uuid", "name"), complete);
+        }
+        if(args.length == 3 && args[1].equalsIgnoreCase("name")){
+            List<String> players = new ArrayList<>();
+            Bukkit.getOnlinePlayers().forEach(p -> players.add(p.getName()));
+            StringUtil.copyPartialMatches(args[2], players, complete);
+        }
+        Collections.sort(complete);
+        return complete;
+    }
+}
---
+++ src/main/java/de/petropia/turtleServer/server/user/PetropiaPlayer.java
@@ -0,0 +1,172 @@
+package de.petropia.turtleServer.server.user;
+
+import de.petropia.turtleServer.server.TurtleServer;
+import dev.morphia.annotations.*;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Entity(value = "players")
+public class PetropiaPlayer {
+
+    @Id
+    private ObjectId id;
+    @Indexed(options = @IndexOptions(unique = true))
+    private String uuid;
+    @Indexed
+    private String userName;
+    private String skinTexture;
+    private String skinTextureSignature;
+    private boolean online;
+    private String server;
+    private int lastOnline;
+    @Property(value = "name_history")
+    private List<String> nameHistory = new ArrayList<>();
+
+    /**
+     * A PetropiaPlayer is a player profile for the petropia network.
+     * Every player who joined the network at least once is stored in a MongoDB Database.
+     * The Petropia player is synchronized with the database when the player joins, so there
+     * is no need to do it manually.
+     */
+    public PetropiaPlayer(){
+
+    }
+
+    /**
+     * @return The database id of this {@link PetropiaPlayer} object
+     */
+    public ObjectId getId() {
+        return id;
+    }
+
+    /**
+     * @return Mojang UUID of this player
+     */
+    public String getUuid() {
+        return uuid;
+    }
+
+    /**
+     * Set the uuid of a player </br>
+     * <b>This is internal use only. Changing the uuid can break the plugin</b>
+     */
+    public void setUuid(String uuid){
+        this.uuid = uuid;
+    }
+
+    /**
+     * @return Last known username.
+     * If the player was not online since he changes his name, this name won't be updated
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @return The current base64 encoded texture of his skin
+     * If the player was not online since he changes his name, this name won't be updated
+     */
+    public String getSkinTexture() {
+        return skinTexture;
+    }
+
+    /**
+     * Update players skin texture in base64 encoding
+     * @param base64Texture Player texture
+     */
+    public void updateSkinTexture(String base64Texture){
+        this.skinTexture = base64Texture;
+    }
+
+    /**
+     * @return True when player is online on the network
+     */
+    public boolean isOnline() {
+        return online;
+    }
+
+    /**
+     * Update the players current online status on the network.
+     * This is not meant for the api, internal use only
+     * @param online false when offline
+     */
+    public void updateOnline(boolean online) {
+        this.online = online;
+    }
+
+    /**
+     * @return Current server the player is online. Null if not online
+     */
+    public String getServer() {
+        if(server.equalsIgnoreCase("null")){
+            return null;
+        }
+        return server;
+    }
+
+    /**
+     * Update the current server from the player
+     * This is not meant for the api, internal use only
+     * @param server Current server
+     */
+    public void updateServer(String server) {
+        this.server = server;
+    }
+
+    /**
+     * @return Last logout time as Unix rime stamp
+     */
+    public int getLastOnline() {
+        return lastOnline;
+    }
+
+    /**
+     * Update the last logout time as unix timestamp
+     * This is not meant for the api, internal use only
+     * @param lastOnline lastLogout time as unix timestamp
+     */
+    public void updateLastOnline(int lastOnline) {
+        this.lastOnline = lastOnline;
+    }
+
+    /**
+     * @return List of all names a player had on this server
+     */
+    public List<String> getNameHistory() {
+        return new ArrayList<>(nameHistory);
+    }
+
+    /**
+     * Update the username. Remember to {@link PetropiaPlayer#updatePlayer()} to save changes
+     * @param userName New username of player
+     */
+    public void updateUserName(String userName){
+        nameHistory.add(userName);
+        this.userName = userName;
+    }
+
+    /**
+     * @return Signature of the players skin
+     */
+    public String getSkinTextureSignature() {
+        return skinTextureSignature;
+    }
+
+    /**
+     * Updated the skin
+     * @param skinTextureSignature Signature of players skin
+     */
+    public void updateSkinTextureSignature(String skinTextureSignature) {
+        this.skinTextureSignature = skinTextureSignature;
+    }
+
+    /**
+     * Updated the player on the Database
+     */
+    public CompletableFuture<PetropiaPlayer> updatePlayer(){
+        return TurtleServer.getMongoDBHandler().savePlayer(this);
+    }
+}
