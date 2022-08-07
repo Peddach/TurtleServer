833cdc08935b28fe563f74dd38b2ce17ea1d57c7
ProfessorSam <Samderprofessor@gmail.com>
2022-08-07 14:13:53 +0200

    Comments

--- src/main/java/de/petropia/turtleServer/api/util/TimeUtil.java
+++ src/main/java/de/petropia/turtleServer/api/util/TimeUtil.java
@@ -6,6 +6,11 @@ import java.util.Date;
 
 public class TimeUtil {
 
+    /**
+     * Convert a unix timestamp to a human-readable string in format dd.MM.yyyy HH:mm
+     * @param timestamp Unix timestamp second of epoche
+     * @return Human-readable String (dd.MM.yyyy HH:mm)
+     */
     public static String unixTimestampToString(int timestamp){
         Date date = Date.from(Instant.ofEpochSecond(timestamp));
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
