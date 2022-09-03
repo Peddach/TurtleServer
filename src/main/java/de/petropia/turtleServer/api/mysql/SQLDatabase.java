756b54435ffb640b55d7dde5c6c246cbd3e3d338
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2022-09-03 17:17:34 +0200

    Database improvements

--- src/main/java/de/petropia/turtleServer/api/mysql/SQLDatabase.java
+++ src/main/java/de/petropia/turtleServer/api/mysql/SQLDatabase.java
@@ -15,52 +15,37 @@ import java.util.List;
 public class SQLDatabase {
 
     private final PetropiaPlugin plugin;
-    private Connection con;
-    private boolean isConnected;
+
+    private final String address;
+    private final String port;
+    private final String name;
+    private final String username;
+    private final String password;
+
 
     public SQLDatabase(PetropiaPlugin plugin) {
         this.plugin = plugin;
+        address = plugin.getConfig().getString("Database.Address");
+        port = plugin.getConfig().getString("Database.Port");
+        name = plugin.getConfig().getString("Database.Name");
+        username = plugin.getConfig().getString("Database.Username");
+        password = plugin.getConfig().getString("Database.Password");
     }
 
-    /**
-     * Connects to the database
-     */
-    public void connect(){
-        String address = plugin.getConfig().getString("Database.Address");
-        String port = plugin.getConfig().getString("Database.Port");
-        String database = plugin.getConfig().getString("Database.Name");
-        String username = plugin.getConfig().getString("Database.Username");
-        String password = plugin.getConfig().getString("Database.Password");
-
-        if(!isConnected){
-            try{
-                con = DriverManager.getConnection("jdbc:mysql://" + address + ":" + port + "/" + database + "?autoReconnect=true", username, password);
-                isConnected = true;
-                plugin.getMessageUtil().showDebugMessage("§bSuccessfully connected to database!");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+    private Connection getConnection(){
+        try{
+            return DriverManager.getConnection("jdbc:mysql://" + address + ":" + port + "/" + name + "?autoReconnect=true", username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    }
-
-    /**
-     * Disconnects from the database
-     */
-    public void disconnect(){
-        if(isConnected){
-            try {
-                con.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        return null;
     }
 
     /**
      * Creates the table, that contains all arenas and their values in the database
      */
     public void createArenasTable(){
-        try{
+        try(Connection con = getConnection()){
             con.prepareStatement("CREATE TABLE IF NOT EXISTS arenas (server VARCHAR(100), name VARCHAR(100), state VARCHAR(100), playerCount INT(16))").executeUpdate();
             plugin.getMessageUtil().showDebugMessage("§bSuccessfully created arena-table!");
         } catch (SQLException e) {
@@ -72,7 +57,7 @@ public class SQLDatabase {
      * Creates the table, that contains the arena, a joining player should be put in the database
      */
     public void createJoiningPlayersTable(){
-        try{
+        try(Connection con = getConnection()){
             con.prepareStatement("CREATE TABLE IF NOT EXISTS joiningPlayers (server VARCHAR(100), name VARCHAR(100), arena VARCHAR(100))").executeUpdate();
             plugin.getMessageUtil().showDebugMessage("§bSuccessfully created joiningPlayers-table!");
         } catch (SQLException e) {
@@ -88,7 +73,7 @@ public class SQLDatabase {
         if(plugin instanceof PetropiaMinigame minigame) {
             if (!minigame.getArenas().contains(arena)) {
                 Bukkit.getScheduler().runTaskAsynchronously(minigame, () -> {
-                    try {
+                    try(Connection con = getConnection()){
                         PreparedStatement st = con.prepareStatement("INSERT INTO arenas (server, name, state, playerCount) VALUES (?,?,?,?)");
                         st.setString(1, plugin.getCloudNetAdapter().getServerInstanceName());
                         st.setString(2, arena.getName());
@@ -101,7 +86,7 @@ public class SQLDatabase {
                 });
             } else {
                 Bukkit.getScheduler().runTaskAsynchronously(minigame, () -> {
-                    try {
+                    try(Connection con = getConnection()){
                         PreparedStatement st = con.prepareStatement("UPDATE arenas SET server = ?, state = ?, playerCount = ? WHERE name = ?");
                         st.setString(4, arena.getName());
                         st.setString(1, plugin.getCloudNetAdapter().getServerInstanceName());
@@ -121,7 +106,7 @@ public class SQLDatabase {
      * @param arena the arena to delete
      */
     public void deleteArena(Arena arena){
-        try {
+        try(Connection con = getConnection()){
             PreparedStatement st = con.prepareStatement("DELETE FROM arenas WHERE server = ? AND name = ?");
             st.setString(1, plugin.getCloudNetAdapter().getServerInstanceName());
             st.setString(2, arena.getName());
@@ -137,7 +122,7 @@ public class SQLDatabase {
      * @return The arena that the joining player should be put in
      */
     public Arena getJoiningPlayerArena(PetropiaMinigame minigame, Player player){
-        try {
+        try(Connection con = getConnection()){
             PreparedStatement st = con.prepareStatement("SELECT arena FROM joiningPlayers WHERE server = ? AND name = ?");
             st.setString(1, plugin.getCloudNetAdapter().getServerInstanceName());
             st.setString(2, player.getName());
@@ -173,7 +158,7 @@ public class SQLDatabase {
 
             plugin.getMessageUtil().sendMessage(player, Component.text("Connecting..."));
 
-            try{
+            try(Connection con = getConnection()){
                 PreparedStatement st = con.prepareStatement("INSERT INTO joiningPlayers (server, name, arena) VALUES (?, ?, ?)");
                 st.setString(1, arenaServer);
                 st.setString(2, player.getName());
@@ -193,7 +178,7 @@ public class SQLDatabase {
      */
     public void removeJoiningPlayer(Player player){
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            try{
+            try(Connection con = getConnection()){
                 PreparedStatement st = con.prepareStatement("DELETE FROM joiningPlayers WHERE name = ?");
                 st.setString(1, player.getName());
                 st.executeUpdate();
@@ -207,7 +192,7 @@ public class SQLDatabase {
      * Deletes remaining data, that might not have been deleted, when the server was closed
      */
     public void deleteRemainingData(){
-        try {
+        try(Connection con = getConnection()){
             PreparedStatement st1 = con.prepareStatement("DELETE FROM arenas WHERE server = ?");
             PreparedStatement st2 = con.prepareStatement("DELETE FROM joiningPlayers WHERE server = ?");
 
@@ -230,7 +215,7 @@ public class SQLDatabase {
         List<String[]> arenaDataArrays = new ArrayList<>();
 
         //Get Arenas from database
-        try{
+        try(Connection con = getConnection()){
             PreparedStatement st = con.prepareStatement("SELECT * FROM arenas");
             ResultSet rs = st.executeQuery();
             while (rs.next()){
