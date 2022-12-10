b1107b2765d467108048a58da994dd34e5274258
ProfessorSam <Samderprofessor@gmail.com>
2022-12-10 14:51:18 +0100

    Delete World locally

--- src/main/java/de/petropia/turtleServer/server/commands/WorldCommand.java
+++ src/main/java/de/petropia/turtleServer/server/commands/WorldCommand.java
@@ -11,47 +11,87 @@ import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class WorldCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WorldCommand implements CommandExecutor, TabExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(!sender.hasPermission("TurtleServer.WorldCommand")){
+        if (!sender.hasPermission("TurtleServer.WorldCommand")) {
             return false;
         }
-        if(!(sender instanceof Player player)){
+        if (!(sender instanceof Player player)) {
             return false;
         }
-        if(args.length < 1){
+        if (args.length < 1) {
             TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("Argumente: copy, load, save", NamedTextColor.GRAY));
             return false;
         }
-        if(args[0].equalsIgnoreCase("deleteLocal")){
+        if (args[0].equalsIgnoreCase("list")) {
+            Component message = Component.text("Aktuell sind folgende Welten geladen: ", NamedTextColor.GRAY);
+            for(int i = 0; i < Bukkit.getWorlds().size(); i++){
+                World world = Bukkit.getWorlds().get(i);
+                Component worldMsg = Component.text(world.getName(), NamedTextColor.GREEN)
+                        .hoverEvent(Component.empty().toBuilder().append(
+                                Component.text("Env: " + world.getEnvironment(), NamedTextColor.GRAY),
+                                Component.newline(),
+                                Component.text("Loaded Chunks: " + world.getLoadedChunks().length, NamedTextColor.GRAY),
+                                Component.newline(),
+                                Component.text("Players: " + world.getPlayers().size(), NamedTextColor.GRAY)).asComponent())
+                        .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.RUN_COMMAND, "/world tp " + world.getName()));
+                message = message.append(worldMsg);
+                if(i + 1 < Bukkit.getWorlds().size()){
+                    message = message.append(Component.text(", ", NamedTextColor.GRAY));
+                }
+            }
+            TurtleServer.getInstance().getMessageUtil().sendMessage(player, message);
+        }
+        if(args[0].equalsIgnoreCase("tp")){
             if(args.length != 2){
+                TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Bitte gib eine Welt an!", NamedTextColor.RED));
+                return false;
+            }
+            World world = Bukkit.getWorld(args[1]);
+            if(world == null){
+                TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Welte wurde nicht gefunden", NamedTextColor.RED));
+                return false;
+            }
+            TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Du wirst gleich teleportiert!", NamedTextColor.GREEN));
+            player.teleportAsync(world.getSpawnLocation());
+        }
+        if (args[0].equalsIgnoreCase("deleteLocal")) {
+            if (args.length != 2) {
                 TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Bitte gib eine Welt an!"));
                 return false;
             }
-            TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("Bist du sicher, dass die Welt aus der Datenbank gelöscht werden soll?", NamedTextColor.RED));
+            TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("Bist du sicher, dass die Welt vom aktuellen Server gelöscht werden soll?", NamedTextColor.RED));
             TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("Diese Aktion ist irreversibel!", NamedTextColor.DARK_RED));
             TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("[✔]", NamedTextColor.GREEN).clickEvent(ClickEvent.clickEvent(
                     ClickEvent.Action.RUN_COMMAND, "/world deleteLocalyes " + args[1]
             )));
         }
-        if(args[0].equalsIgnoreCase("deleteLocalyes")){
-            TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Lösche Welt" + args[1]));
+        if (args[0].equalsIgnoreCase("deleteLocalyes")) {
+            TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Lösche Welt: " + args[1], NamedTextColor.RED));
             WorldManager.deleteLocalWorld(Bukkit.getWorld(args[1]));
         }
-        if(args[0].equalsIgnoreCase("save")){
+        if (args[0].equalsIgnoreCase("save")) {
             TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("Bist du dir sicher, dass die Welt in der du dich befindest gespeichert werden soll in der Datenbank?", NamedTextColor.RED));
             TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("Diese Aktion ist irreversibel!", NamedTextColor.DARK_RED));
             TurtleServer.getInstance().getMessageUtil().sendMessage(sender, Component.text("[✔]", NamedTextColor.GREEN).clickEvent(ClickEvent.clickEvent(
                     ClickEvent.Action.RUN_COMMAND, "/world saveyes"
             )));
         }
-        if(args[0].equalsIgnoreCase("saveyes")){
+        if (args[0].equalsIgnoreCase("saveyes")) {
             World worldToSave = player.getWorld();
             World.Environment worldToSaveEnv = worldToSave.getEnvironment();
             ChunkGenerator worldToSaveGenerator = worldToSave.getGenerator();
@@ -61,7 +101,7 @@ public class WorldCommand implements CommandExecutor {
             }
             try {
                 WorldManager.saveToDBWorld(worldToSave).thenAccept(bool -> {
-                    if(bool){
+                    if (bool) {
                         TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Welt erfolgreich gespeichert!", NamedTextColor.GREEN));
                     } else {
                         TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Fehler beim speichern der Welt!", NamedTextColor.RED));
@@ -82,13 +122,13 @@ public class WorldCommand implements CommandExecutor {
                                 throwable.printStackTrace();
                                 return null;
                             }).thenAccept(success -> {
-                                if(!success){
+                                if (!success) {
                                     TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Async teleport failed! Teleport sync", NamedTextColor.RED));
                                     player.teleport(newSpawn);
                                     newWorld.setSpawnLocation(newSpawn);
                                 }
                             });
-                        }, 5*20);
+                        }, 5 * 20);
                     });
                 });
             } catch (ZipException e) {
@@ -96,8 +136,8 @@ public class WorldCommand implements CommandExecutor {
                 TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Es ist ein Fehler aufgetreten! " + e.getMessage(), NamedTextColor.RED));
             }
         }
-        if(args[0].equalsIgnoreCase("load")){
-            if(args.length != 3){
+        if (args[0].equalsIgnoreCase("load")) {
+            if (args.length != 3) {
                 TurtleServer.getInstance().getMessageUtil().sendMessage(player, Component.text("Bitte gib eine id der Welt an und danach dessen Name wenn sie geladen wird!"));
                 return false;
             }
@@ -110,5 +150,17 @@ public class WorldCommand implements CommandExecutor {
             });
         }
         return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        final List<String> tab = new ArrayList<>();
+        if (args.length == 1) {
+            StringUtil.copyPartialMatches(args[0], Arrays.asList("save", "deleteLocal", "load", "list", "tp"), tab);
+        }
+        if (args.length == 2 && (args[0].equalsIgnoreCase("deleteLocal") || args[0].equalsIgnoreCase("tp"))) {
+            StringUtil.copyPartialMatches(args[1], Bukkit.getWorlds().stream().map(World::getName).collect(Collectors.toList()), tab);
+        }
+        return tab;
     }
 }
